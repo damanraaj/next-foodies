aa80035cefd6c872211bbc7965521baa93365e8d
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2023-12-17 17:43:10 +0530

    update meals home page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
 const MealsHomePage = () => {
   return (
-    <div>MealsHomePage</div>
-  )
-}
+    <div>
+      <header className={classes["header"]}>
+        <h1>
+          Delicious meals, created
+          <span className={classes.highlight}> by you.</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share your favorite recipe.</Link>
+        </p>
+      </header>
+      <main className={classes["main"]}></main>
+    </div>
+  );
+};
 
-export default MealsHomePage
+export default MealsHomePage;
