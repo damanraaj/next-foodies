40ef523d3ee1134a7cb59d3e905ddf1503d5ab5e
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2023-12-20 21:36:36 +0530

    add custom 404 page

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => {
+  return <main className="not-found">
+    <h1>Not Found</h1>
+    <p>We could not find the requested page or resource.</p>
+  </main>
+};
+
+export default NotFound;
