59dfb905582b37b2dfb855878228431b23884a1b
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2023-12-22 11:18:41 +0530

    add 404 for meal recipe page

---
+++ app/meals/[slug]/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = () => {
+  return <main className="not-found">
+    <h1>Meal not Found</h1>
+    <p>We could not find the requested recipe.</p>
+  </main>
+};
+
+export default NotFound;
